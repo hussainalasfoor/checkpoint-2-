0eaeb6bcd55f9bcdafd893b697fba84287473de0
hussainalasfoor <81930847+hussainalasfoor@users.noreply.github.com>
2021-04-09 17:01:29 +0300

    Create answerq6.py

---
+++ answerq6.py
@@ -0,0 +1,8 @@
+import numpy as np
+x = np.array([0, 1, 2])
+y = np.array([2, 1, 0])
+print("Original array1:")
+print(x)
+print("Original array1:")
+print(y)
+print(f"Covariance matrix of the said arrays: {np.cov(x, y)")
