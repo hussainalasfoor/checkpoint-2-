c8d2a31e4c03a6da5db80565c507bc94893ec8ed
hussainalasfoor <81930847+hussainalasfoor@users.noreply.github.com>
2021-04-09 15:34:50 +0300

    Update answerq1.py

--- answerq1.py
+++ answerq1.py
@@ -1,3 +1,8 @@
-list = [ x for x in range(1999,3201) if  x % 7 == 0 and  x % 5 != 0 ] 
-print(list)
-
+li = []
+for x in range(1999,3201):
+    if  x % 7 == 0 and  x % 5 != 0:
+        # print(x)
+        list.append(li, x)
+       
+        
+print(li)
